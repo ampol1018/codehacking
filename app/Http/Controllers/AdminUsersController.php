f88c8b212112f62d6a5158f723a51ed5f996eea8
Jp <jaypee.ampol0628@gmail.com>
2016-08-18 15:27:30 +0800

    Users Working 100% with everything except security

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Requests\UsersEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use Illuminate\Http\Request;
@@ -50,16 +51,22 @@ class AdminUsersController extends Controller
     public function store(UsersRequest $request)
     {
 
-        $input = $request->all();
+        if(trim($request->password) == ''){
+            $input = $request->except('password');
+        } else {
+            $input['password'] = bcrypt($request->password);
+            $input = $request->all();
+        }
+
 
         if ($file = $request->file('photo_id')){
             $name = time() . $file->getClientOriginalName();
             $file->move('images',$name);
             $photo = Photo::create(['file'=>$name]);
-            $input['photo_id'] = $photo->id;    
+            $input['photo_id'] = $photo->id;
         }
 
-        $input['password'] = bcrypt($request->password);
+
         User::create($input);
 
         return redirect('/admin/users');
@@ -85,7 +92,11 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        return view('admin.users.edit');
+
+        $user = User::findOrFail($id);
+        $roles = Role::lists('name','id')->all();
+
+        return view('admin.users.edit',compact('user','roles'));
     }
 
     /**
@@ -95,9 +106,30 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
-        //
+
+        $user = User::findOrFail($id);
+
+        if(trim($request->password) == ''){
+            $input = $request->except('password');
+        } else {
+            $input['password'] = bcrypt($request->password);
+            $input = $request->all();
+        }
+
+
+
+        if($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images',$name);
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+
+        $user->update($input);
+        return redirect('/admin/users');
+
     }
 
     /**
--- app/Photo.php
+++ app/Photo.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     protected $fillable = ['file'];
+
+    //for accessors
+    protected $uploads = '/images/';
+
+
+    public function getFileAttribute($photo){
+        return $this->uploads . $photo;
+    }
+
+
 }
