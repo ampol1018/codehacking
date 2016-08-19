ce8e2dde3a6f0032719c82ea002ce780de179b3a
Jp <jaypee.ampol0628@gmail.com>
2016-08-19 14:49:12 +0800

    able to create post with static category

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -63,7 +63,10 @@
 
                 <ul class="dropdown-menu" role="menu">
                     <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                    <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
                 </ul>
+
+
             </li>
 
 
@@ -150,11 +153,11 @@
                         <a href="#"><i class="fa fa-wrench fa-fw"></i> Posts<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
-                                <a href="/posts">All Posts</a>
+                                <a href="{{route('admin.posts.index')}}">All Posts</a>
                             </li>
 
                             <li>
-                                <a href="/posts/create">Create Post</a>
+                                <a href="{{route('admin.posts.create')}}">Create Post</a>
                             </li>
 
                         </ul>
